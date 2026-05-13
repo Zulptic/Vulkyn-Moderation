9e96dae11c72e9e4225469336bf5e731c30f0b31
Zulptic <allegriadel24@gmail.com>
2026-05-12 20:49:19 -0400

    Distinguish between category and channel

--- src/events/logging/channelCreate.js
+++ src/events/logging/channelCreate.js
@@ -1,6 +1,13 @@
+import { ChannelType } from 'discord.js';
 import { loggingService } from '../../services/loggingService.js';
 
 export default {
     name: 'channelCreate',
-    execute: (channel, client) => loggingService.channelCreate(channel, client),
+    execute: (channel, client) => {
+        if (channel.type === ChannelType.GuildCategory) {
+            loggingService.categoryCreate(channel, client);
+        } else {
+            loggingService.channelCreate(channel, client);
+        }
+    },
 };
--- src/events/logging/channelDelete.js
+++ src/events/logging/channelDelete.js
@@ -1,6 +1,13 @@
+import { ChannelType } from 'discord.js';
 import { loggingService } from '../../services/loggingService.js';
 
 export default {
     name: 'channelDelete',
-    execute: (channel, client) => loggingService.channelDelete(channel, client),
+    execute: (channel, client) => {
+        if (channel.type === ChannelType.GuildCategory) {
+            loggingService.categoryDelete(channel, client);
+        } else {
+            loggingService.channelDelete(channel, client);
+        }
+    },
 };

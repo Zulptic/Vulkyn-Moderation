b6674fa05d21806617cfb5d9bb984d2405157ded
Zulptic <allegriadel24@gmail.com>
2026-05-12 20:48:43 -0400

    Fix the random banExpiry and muteExpiry timeout

--- src/index.js
+++ src/index.js
@@ -40,6 +40,8 @@ const db = new Pool({
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000,
+    keepAlive: true,
+    keepAliveInitialDelayMillis: 10000,
 });
 
 db.on('error', (err) => logger.error('PostgreSQL pool error:', err));
